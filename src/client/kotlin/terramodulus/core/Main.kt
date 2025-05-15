f880544a24b701bd28aac5a3af840768ae7ae844
Ben Forge <74168521+BenCheung0422@users.noreply.github.com>
2025-05-16 06:56:12 +0800

    Draft main structure

---
+++ src/client/kotlin/terramodulus/core/Main.kt
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: 2025 TerraModulus Team and Contributors
+ * SPDX-License-Identifier: LGPL-3.0-only
+ */
+
+package terramodulus.core
+
+fun main() {
+	val game = TerraModulus()
+	try {
+		game.run()
+	} catch (e: Exception) {
+		TODO("Not yet implemented")
+	}
+}
